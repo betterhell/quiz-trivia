93ee8eb84a87c187bfa5442248c12b6a672d14a5
Dmitriy betterHell <skkogame@gmail.com>
2022-12-29 12:32:52 +0300

    adaptive fixes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     },[questions.length])
 
   return (
-    <div className="w-screen h-screen flex m-auto justify-center items-center bg-[url('./assets/background.png')] bg-cover bg-no-repeat bg-center">
+    <div className="pt-[50px] w-screen h-screen flex justify-center items-center bg-[url('./assets/background.png')] bg-cover bg-no-repeat bg-center">
         {gameOver ? <Result /> : <QuizCard /> }
     </div>
   );
