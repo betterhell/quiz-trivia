d68415df99e96c587aea80258b1e487e82350227
Dmitriy betterHell <skkogame@gmail.com>
2022-12-29 12:45:54 +0300

    cleaning code

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -12,20 +12,20 @@ const QuizCard = ({gameName}) => {
     } = useQuizStore()
 
     return (
-            <div className="card m-3 max-w-[464px] w-screen h-fit bg-white rounded-[24px] relative">
-                <header className="hidden sm:flex sm:gap-[77px] sm:justify-between sm:items-center absolute sm:top-[-80px]">
-                    <h1 className="sm:font-[700] sm:text-4xl sm:text-[#F2F2F2] sm:mb-[10px]">{gameName}</h1>
-                    <div className="hidden sm:block">
-                        <CardImg />
-                    </div>
-                </header>
-                <main className="sm:mt-[50px] mt-[10px] p-5">
-                    {startGame ? <QuizQuestion /> : <QuizStart /> }
-                </main>
-                <footer className="p-5 flex justify-end">
-                    {checkAnswer ? <button className="py-3 px-6 text-white rounded-[12px] bg-[#F9A826]" onClick={handleIndexIncrement}>Next</button> : null}
-                </footer>
-            </div>
+        <div className="card m-3 max-w-[464px] w-screen h-fit bg-white rounded-[24px] relative">
+            <header className="hidden sm:flex sm:gap-[77px] sm:justify-between sm:items-center absolute sm:top-[-80px]">
+                <h1 className="sm:font-[700] sm:text-4xl sm:text-[#F2F2F2] sm:mb-[10px]">{gameName}</h1>
+                <div className="hidden sm:block">
+                    <CardImg />
+                </div>
+            </header>
+            <main className="sm:mt-[50px] mt-[10px] p-5">
+                {startGame ? <QuizQuestion /> : <QuizStart /> }
+            </main>
+            <footer className="p-5 flex justify-end">
+                {checkAnswer ? <button className="py-3 px-6 text-white rounded-[12px] bg-[#F9A826]" onClick={handleIndexIncrement}>Next</button> : null}
+            </footer>
+        </div>
     );
 };
 
--- src/components/QuizStart/QuizStart.jsx
+++ src/components/QuizStart/QuizStart.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import {useQuizStore} from "../../store/store";
 
 const QuizStart = () => {
-    const {
-        getAllQuestions,
-        handleStartGame,
-    } = useQuizStore()
-
+    const {getAllQuestions, handleStartGame} = useQuizStore()
 
     const startGame = () => {
         handleStartGame()
@@ -14,12 +10,10 @@ const QuizStart = () => {
     }
 
     return (
-        <>
-            <div className="flex flex-col gap-[15px]">
-                <h2 className="text-xl font-semibold text-[#1D355D] text-center">Trivia is a type of game in which player are asked questions about different topics and they have to get as many correct answers as possible</h2>
-                <button onClick={startGame} className="py-3 px-10 text-white self-center rounded-[12px] bg-[#F9A826] shadow-2xl animate-[wiggle_1.5s_ease-in-out_infinite]">Start</button>
-            </div>
-        </>
+        <div className="flex flex-col gap-[15px]">
+            <h2 className="text-xl font-semibold text-[#1D355D] text-center">Trivia is a type of game in which player are asked questions about different topics and they have to get as many correct answers as possible</h2>
+            <button onClick={startGame} className="py-3 px-10 text-white self-center rounded-[12px] bg-[#F9A826] shadow-2xl animate-[wiggle_1.5s_ease-in-out_infinite]">Start</button>
+        </div>
     );
 };
 
